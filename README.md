d4d173c862bab3d9ecf66cb610c20df4ea6c02dd
haie.yang <haie.yang@alo7.com>
2017-12-11 18:46:52 +0800

    create react project and configure the environment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # React-demo
 only for personal study
+
+# need to know how to edit README
+
+# need to write myself experience sharing
+
+# how to configure the React environment
+
+# the problem had meet
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div
+        style={{ fontSize: '40px', color: '#FF0000', textAlign: 'center', width: '100%', height: '55px', backgroundColor: '#000FFF'}}>
+        this is my first project for react
+      </div>
+    )
+  }
+}
